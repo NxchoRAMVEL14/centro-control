6d80f56771f0c84168de5cc17d147c33092f2527
NxchoRAMVEL14 <osirv92@gmail.com>
2026-07-17 09:49:35 -0600

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,33 @@
+const CACHE = "cc-v1-2";
+const CORE = ["./", "./index.html", "./manifest.webmanifest", "./icon-192.png", "./icon-512.png"];
+self.addEventListener("install", (e) => {
+  e.waitUntil(caches.open(CACHE).then((c) => c.addAll(CORE)).then(() => self.skipWaiting()));
+});
+self.addEventListener("activate", (e) => {
+  e.waitUntil(
+    caches.keys()
+      .then((ks) => Promise.all(ks.filter((k) => k !== CACHE).map((k) => caches.delete(k))))
+      .then(() => self.clients.claim())
+  );
+});
+self.addEventListener("fetch", (e) => {
+  const req = e.request;
+  if (req.method !== "GET") return;
+  if (req.mode === "navigate") {
+    e.respondWith(
+      fetch(req).then((r) => {
+        const cp = r.clone();
+        caches.open(CACHE).then((c) => c.put("./index.html", cp));
+        return r;
+      }).catch(() => caches.match("./index.html"))
+    );
+    return;
+  }
+  e.respondWith(
+    caches.match(req).then((hit) => hit || fetch(req).then((r) => {
+      const cp = r.clone();
+      caches.open(CACHE).then((c) => c.put(req, cp));
+      return r;
+    }))
+  );
+});
